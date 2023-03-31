4da5a58ff99f71b2fc578e3e96092575b1e39990
Barbara Caroline Ribeiro <CIANDT\barbararibeiro@lnb035224cps.ciandt.global>
2023-03-31 01:11:22 -0300

    Desafio Java Week

--- src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
+++ src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.linuxtips.challengejavaweek.controller;
 
 import com.linuxtips.challengejavaweek.model.Produto;
 import com.linuxtips.challengejavaweek.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,5 +14,36 @@ import java.util.List;
 @RequestMapping("/api/")
 public class ProdutoController {
 
+    @Autowired
+    ProdutoService produtoService;
+    @GetMapping("/produtos")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Produto> listarProduto() {
+        return produtoService.listarProdutos();
+    }
+
+    @PostMapping("/produtos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto criarProduto(@RequestBody Produto produto) {
+        return produtoService.criarProduto(produto);
+    }
+
+    @GetMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Produto> buscarProdutoPeloId(@PathVariable(value = "id") Long id) {
+        return produtoService.buscarProdutoPeloId(id);
+    }
+
+    @PutMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Produto> atualizarProdutoPeloId(@PathVariable(value = "id") Long id, @RequestBody Produto produto) {
+        return produtoService.atualizarProdutoPeloId(produto, id);
+    }
+
+    @DeleteMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> apagarPeloId(@PathVariable(value = "id") Long id) {
+        return produtoService.apagarPeloId(id);
+    }
     
 }
